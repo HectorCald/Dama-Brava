814402a5f73b9ccd156bd45bfa18798989b68524
Hector Cald <hi.hector20@gmail.com>
2024-11-24 11:43:42 -0400

    cambios de redireccion en www.

--- index.js
+++ index.js
@@ -57,6 +57,19 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+// Redirigir de http://damabrava.com a https://www.damabrava.com
+app.use((req, res, next) => {
+    // Si el dominio no tiene el prefijo www, lo agregamos y redirigimos
+    if (req.headers.host === 'damabrava.com') {
+        return res.redirect(301, `https://www.damabrava.com${req.url}`);
+    }
+    // Redirigir cualquier versión http:// a https://
+    if (req.protocol === 'http') {
+        return res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
+
 app.use(session({
     secret: 'mi-secreto', // Cambia esto por una clave secreta en producción
     resave: false,
@@ -76,18 +89,7 @@ const verificarAutenticacion = (req, res, next) => {
     }
 };
 
-// Redirigir de http://damabrava.com a https://www.damabrava.com
-app.use((req, res, next) => {
-    // Si el dominio no tiene el prefijo www, lo agregamos y redirigimos
-    if (req.headers.host === 'damabrava.com') {
-        return res.redirect(301, `https://www.damabrava.com${req.url}`);
-    }
-    // Redirigir cualquier versión http:// a https://
-    if (req.protocol === 'http') {
-        return res.redirect(301, `https://${req.headers.host}${req.url}`);
-    }
-    next();
-});
+
 
 
 // Rutas para renderizar las vistas
