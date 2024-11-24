3b8aef18813bf3b9fdc67ec5f1c36b3ad49dd742
Hector Cald <hi.hector20@gmail.com>
2024-11-24 12:02:22 -0400

    cambios de redireccion en www.

--- index.js
+++ index.js
@@ -57,24 +57,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.use((req, res, next) => {
-    // Comprobamos si ya estamos en https y con www
-    if (req.protocol !== 'https' || !req.headers.host.startsWith('www.')) {
-        // Si el dominio no tiene www (damabrava.com en lugar de www.damabrava.com)
-        if (req.headers.host === 'damabrava.com') {
-            // Redirigir de http://damabrava.com a https://www.damabrava.com
-            return res.redirect(301, `https://www.damabrava.com${req.url}`);
-        }
-
-        // Si la solicitud es http y tiene www, redirigir a https://
-        if (req.protocol === 'http') {
-            return res.redirect(301, `https://${req.headers.host}${req.url}`);
-        }
-    }
-
-    // Si la solicitud ya es correcta, continuamos con el siguiente middleware
-    next();
-});
 
 app.use(session({
     secret: 'mi-secreto', // Cambia esto por una clave secreta en producción
